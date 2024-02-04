4617eb6dad2a66b4400f042f508cff2a75e251b8
monkeyDugi <lbd4946@gmail.com>
2024-02-04 16:45:42 +0900

    feat: vue routing 화면 셋팅 테스트

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/user">UserList</router-link> |
+    <router-link to="/user/findById">UserSelect</router-link> |
+    <router-link to="/user/editById">UserUpdate</router-link> |
+    <router-link to="/user/save">UserCreate</router-link> |
   </nav>
   <router-view/>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,26 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/user',
+    name: 'ListView',
+    component: () => import(/* webpackChunkName: "user" */ '../views/ListView.vue')
+  },
+  {
+    path: '/user/findById',
+    name: 'SelectView',
+    component: () => import(/* webpackChunkName: "user" */ '../views/SelectView.vue')
+  },
+  {
+    path: '/user/editById',
+    name: 'UpdateView',
+    component: () => import(/* webpackChunkName: "user" */ '../views/UpdateView.vue')
+  },
+  {
+    path: '/user/save',
+    name: 'CreateView',
+    component: () => import(/* webpackChunkName: "user" */ '../views/CreateView.vue')
   }
 ]
 
