a8ec75b70a46a1f40802be796c5b48f1ecce0b19
monkeyDugi <lbd4946@gmail.com>
2024-02-04 16:53:59 +0900

    feat: 필요없는 nav 제거

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,4 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/user">UserList</router-link> |
-    <router-link to="/user/findById">UserSelect</router-link> |
-    <router-link to="/user/editById">UserUpdate</router-link> |
-    <router-link to="/user/save">UserCreate</router-link> |
-  </nav>
   <router-view/>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a href="/user">사용자 목록</a>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  name: 'HomeView'
 }
 </script>
+
+<style scoped>
+a {
+  color: black;
+  text-decoration: none;
+  font-weight: bold;
+  font-size: 1.5rem;
+}
+</style>
