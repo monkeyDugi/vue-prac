8046d21b907a82b8a56cfc520ea968bc41c2c96d
monkeyDugi <lbd4946@gmail.com>
2024-02-05 13:40:25 +0900

    feat: 상세 화면에서 남자면 남자만 여자면 여자만 라디오 박스 나오게 하기

--- src/views/SelectView.vue
+++ src/views/SelectView.vue
@@ -15,15 +15,15 @@
 		    <input type="password" class="form-control" id="pwd" placeholder="비밀번호를 입력하세요." name="pwd" readonly v-model="result.pwd"/>
 		  </div>
 			<div class="d-flex">
-			  <div class="p-2 flex-fill">
+			  <div class="p-2 flex-fill" v-if="result.gender">
 			  	<div class="form-check">
-					<input type="radio" class="form-check-input" id="radio1" name="optradio" value="true" checked v-model="result.gender" />
+					<input type="radio" class="form-check-input" id="radio1" name="optradio" v-bind:value="true" checked v-model="result.gender" />
 					<label class="form-check-label" for="radio1">남성</label>
 				</div>
 			  </div>
-			  <div class="p-2 flex-fill">
+			  <div class="p-2 flex-fill" v-else>
 			  	<div class="form-check">
-					<input type="radio" class="form-check-input" id="radio2" name="optradio" value="false" checked v-model="result.gender" />
+					<input type="radio" class="form-check-input" id="radio2" name="optradio" v-bind:value="false" checked v-model="result.gender" />
 					<label class="form-check-label" for="radio2">여성</label>
 				</div>
 			  </div>
